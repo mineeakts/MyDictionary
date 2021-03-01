1de136ec5bcbdf6eeab4d79f96f2373618f6168f
Mine Aktaş <mineeakts@gmail.com>
2021-03-02 01:37:28 +0300

    Proje dosyası ekle.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] keys;
+        TKey[] tempKeys;
+        TValue[] values;
+        TValue[] tempValues;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key,TValue value)
+        {
+            tempKeys = keys;
+            tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempValues.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void List()
+        {
+
+            Console.WriteLine("My Dictionary");
+            for (int i = 0; i < values.Length; i++)
+            {
+                Console.WriteLine(keys[i] + ". " + values[i]);
+            }
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Beyza");
+            myDictionary.Add(2, "Mine");
+            myDictionary.Add(3, "Aktaş");
+
+            myDictionary.List();
+        }
+    }
+}
